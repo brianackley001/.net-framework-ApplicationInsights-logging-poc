9585892cb1042ea49241f3069fae8f9f9c72ba10
brianackley001 <brian.ch.ackley@gmail.com>
2024-08-04 17:49:33 -0700

    Add solution fodlers, placeholder apps

---
+++ ConsoleApp-4.6.2_.NET_Framework/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_4._6._2_.NET_Framework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
